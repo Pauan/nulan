79bb093dfd2ecb0dc38a5f1cd2e2335d9616cce3
Pauan <pcxunlimited@gmail.com>
2012-07-19 19:00:24 -0700

    Dictionaries and sets work now too!

--- nu_tree.py
+++ nu_tree.py
@@ -13,15 +13,6 @@ nil = Nil()
 nil.l = nil
 nil.r = nil
 
-#str(iter_to_tree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-
-#def haskey(self, key):
-#  return False
-#def getkey(self, key, default=w_false):
-#  return default
-#def setkey(self, key, value):
-#  return Dictionary(nil, nil, key, value)
-
 
 # AVL Tree
 class Tree(object):
@@ -30,25 +21,25 @@ class Tree(object):
     self.r     = r
     self.depth = max(l.depth, r.depth) + 1
 
-  def map(self):
+  def ret(self):
     return self
 
   def __iter__(self):
     for x in self.l:
       yield x
-    yield self.map()
+    yield self.ret()
     for x in self.r:
       yield x
 
   def __reversed__(self):
     for x in self.r:
       yield x
-    yield self.map()
+    yield self.ret()
     for x in self.l:
       yield x
 
-  def __repr__(self):
-    return "({!r} {!r})".format(self.l, self.r)
+  #def __repr__(self):
+  #  return "({!r} {!r})".format(self.l, self.r)
 
 
 # Tree with O(1) length and lookup by index
@@ -64,127 +55,72 @@ class Ordered(Tree):
   def __repr__(self):
     return "(%seq {})".format(" ".join(repr(x) for x in self))
 
+  def ret(self):
+    return self.value
+
   #def __str__(self):
   #  return "({} {}={} {})".format(str(self.l), self.value, self.count, str(self.r))
-
-  def map(self):
-    return self.value
 
   def transfer(self, y):
     self.value = y.value
     return self
 
-  def assign(self, value):
+
+# Tree that maps keys to values
+class Dictionary(Ordered):
+  def __str__(self):
+    return "{{{}}}".format(" ".join("{} {}".format(*x) for x in self))
+
+  def __repr__(self):
+    return "(dict {})".format(" ".join("{!r} {!r}".format(*x) for x in self))
+    #result = []
+    #for k, v in self:
+    #  result.append(repr(k))
+    #  result.append(repr(v))
+    #return "(dict {})".format(" ".join(result))
+
+  def ret(self):
+    return self.key, self.value
+
+  def transfer(self, y):
+    self.key   = y.key
+    self.value = y.value
+    return self
+
+  def key_ins(self, key, value):
+    self.key   = key
     self.value = value
     return self
 
 
 # Dictionary where the values are the keys
-class Set(Ordered):
-  def __init__(self, l, r):
-    Ordered.__init__(self, l, r)
-    self.key = value
-
+class Set(Dictionary):
   def __repr__(self):
-    result = []
-    for x in self:
-      result.append(repr(x.key))
-    return "(&set {})".format("".join(result))
+    return "(&set {})".format(" ".join(repr(x) for x in self))
 
-  def haskey(self, key):
-    if key == self.key:
-      return True
-    elif key < self.key:
-      return self.l.haskey(key)
-    else:
-      return self.r.haskey(key)
+  def ret(self):
+    return self.key
 
-  #def getkey(self, key, default=w_false):
-  #  return self.haskey(key) or default
+  def transfer(self, y):
+    self.key   = y.key
+    self.value = y.key
+    return self
 
-  def setkey(self, key, value=None):
-    if self == nil:
-      return Set(nil, nil, key)
-    elif key == self.key:
-      return self
-    elif key < self.key:
-      return Set(self.l.setkey(key, value),
-                 self.r,
-                 self.key)
-    else:
-      return Set(self.l,
-                 self.r.setkey(key, value),
-                 self.key)
+  def key_ins(self, key, value):
+    self.key   = key
+    self.value = key
+    return self
 
-
-class Dictionary(Set):
-  def __init__(self, l, r, key, value):
-    Set.__init__(self, l, r, key)
-    self.value = value
-
-  def __repr__(self):
-    result = []
-    for k, v in self:
-      result.append(repr(k))
-      result.append(repr(v))
-    return "(dict {})".format(" ".join(result))
-
-  def __iter__(self):
-    pass
-
-  def __repr__(self):
-    if self.l == nil:
-      if self.r == nil:
-        return "({}={})".format(self.key, self.value)
-      else:
-        return "({}={} {})".format(self.key, self.value, self.r)
-    elif self.r == nil:
-      return "({} {}={})".format(self.l, self.key, self.value)
-    else:
-      return "({} {}={} {})".format(self.l, self.key, self.value, self.r)
-
-  #def getkey(self, key, default=w_false):
-  #  if key == self.key:
-  #    return self.value
-  #  elif key < self.key:
-  #    return self.l.getkey(key, default=default)
-  #  else:
-  #    return self.r.getkey(key, default=default)
-
-  def setkey(self, key, value):
-    if key == self.key:
-      return Dictionary(self.l, self.r, key, value)
-    elif key < self.key:
-      return Dictionary(self.l.setkey(key, value),
-                        self.r,
-                        self.key,
-                        self.value)
-    else:
-      return Dictionary(self.l,
-                        self.r.setkey(key, value),
-                        self.key,
-                        self.value)
-
-#def size(tree):
-#  if tree is nil:
-#    return 0
-#  else:
-#    return size(tree.l) + size(tree.r) + 1
-
-
-#(0.5 < α < 1)
-
-#height(tree) <= log (1 / α) (NodeCount)
 
 
 # Balanced tree construction
-def bcons(cons, l, r, tree):
+def bcons(cons, tree, l, r):
   if l.depth > r.depth + 1:
     if l.l.depth > l.r.depth:
       return cons(l.l,
                   cons(l.r, r).transfer(tree)).transfer(l)
     else:
-      return cons(cons(l.l,   l.r.l).transfer(l)
+      return cons(cons(l.l,   l.r.l).transfer(l),
                   cons(l.r.r, r    ).transfer(tree)).transfer(l.r)
   elif r.depth > l.depth + 1:
     if r.r.depth > r.l.depth:
@@ -202,78 +138,139 @@ def simple_merge(cons, l, r):
     return r
   elif r == nil:
     return l
-  elif l.depth < b.depth:
-    return bcons(cons, r
-                 simple_merge(l, r.l),
+  elif l.depth < r.depth:
+    return bcons(cons, r,
+                 simple_merge(cons, l, r.l),
                  r.r)
   else:
     return bcons(cons, l,
                  l.l,
-                 simple_merge(l.r, r))
+                 simple_merge(cons, l.r, r))
+
 
 
 def ind_get(tree, ind, default=9001):
   c = tree.l.count
-  #print tree, ind, c
   if tree == nil:
     return default
   elif ind == c:
-    return tree.map()
+    return tree.value
   elif ind < c:  # left branch
     return ind_get(tree.l, ind, default)
-  #elif ind == tree.r.count:
-  #  return tree.map()
   else:          # right branch
-    #print "RIGHT", tree, ind, tree.r.count
     return ind_get(tree.r, ind - c - 1, default)
 
 def ind_ins(cons, tree, ind, value):
+  if not -1 <= ind <= tree.count:
+    raise IndexError(ind)
   c = tree.l.count
-  #print c, ind
   if tree == nil:
-    return cons(nil, nil).assign(value)
+    x = cons(nil, nil)
+    x.value = value
+    return x
   elif ind == c:
-    return cons(tree.l, tree.r).assign(value)
+    x = cons(tree.l, tree.r)
+    x.value = value
+    return x
   elif ind < c:  # left branch
-    return bcons(cons, tree
+    return bcons(cons, tree,
                  ind_ins(cons, tree.l, ind, value),
                  tree.r)
   else:          # right branch
-    return bcons(cons, tree
+    return bcons(cons, tree,
                  tree.l,
                  ind_ins(cons, tree.r, ind - c - 1, value))
 
-def ind_rem(cons, tree, ind, default=9001):
+# TODO provide a default that's returned if deletion fails?
+def ind_rem(cons, tree, ind):
+  if not -1 < ind < tree.count:
+    raise IndexError(ind)
   c = tree.l.count
-  if tree == nil:
-    return default
-  elif ind == c:
+  if ind == c:
     return simple_merge(cons, tree.l, tree.r)
   elif ind < c:  # left branch
-    return bcons(cons, tree
-                 ind_rem(cons, tree.l, ind, value),
+    return bcons(cons, tree,
+                 ind_rem(cons, tree.l, ind),
                  tree.r)
   else:          # right branch
-    return bcons(cons, tree
+    return bcons(cons, tree,
                  tree.l,
-                 ind_rem(cons, tree.r, ind - c - 1, value))
+                 ind_rem(cons, tree.r, ind - c - 1))
+
+
+def key_has(tree, key):
+  if tree == nil:
+    # TODO
+    return False
+  elif key == tree.key:
+    # TODO
+    return True
+  elif key < tree.key:
+    return key_has(tree.l, key)
+  else:
+    return key_has(tree.r, key)
+
+def key_get(tree, key, default=9001):
+  if tree == nil:
+    return default
+  elif key == tree.key:
+    return tree.value
+  elif key < tree.key:  # left branch
+    return key_get(tree.l, key, default)
+  else:                 # right branch
+    return key_get(tree.r, key, default)
+
+def key_ins(cons, tree, key, value):
+  if tree == nil:
+    return cons(nil, nil).key_ins(key, value)
+  elif key == tree.key:
+    return cons(tree.l, tree.r).key_ins(key, value)
+  elif key < tree.key:  # left branch
+    return bcons(cons, tree,
+                 key_ins(cons, tree.l, key, value),
+                 tree.r)
+  else:                 # right branch
+    return bcons(cons, tree,
+                 tree.l,
+                 key_ins(cons, tree.r, key, value))
+
+# TODO provide a default that's returned if deletion fails?
+def key_rem(cons, tree, key):
+  if tree == nil:
+    raise KeyError(key)
+  elif key == tree.key:
+    return simple_merge(cons, tree.l, tree.r)
+  elif key < tree.key:  # left branch
+    return bcons(cons, tree,
+                 key_rem(cons, tree.l, key),
+                 tree.r)
+  else:                 # right branch
+    return bcons(cons, tree,
+                 tree.l,
+                 key_rem(cons, tree.r, key))
+
 
 
 # O(n log2(n))
-def iter_to_tree(xs, cons):
-  xs = iter(xs)
-  y  = nil
-  try:
-    while 1:
-      y = ind_ins(cons, y, y.count, xs.next())
-  except StopIteration:
-    return y
+def iter_to_list(xs, cons=Ordered):
+  y = nil
+  for k in xs:
+    y = ind_ins(cons, y, y.count, k)
+  return y
 
-def iter_to_list(xs):
-  return iter_to_tree(xs, cons=Ordered)
+# O(n log2(n))
+def iter_to_set(xs, cons=Set):
+  y = nil
+  for k in xs:
+    y = key_ins(cons, y, k, k)
+  return y
 
-def iter_to_dict(xs):
-  return iter_to_tree(xs, cons=Dictionary)
+# O(n log2(n))
+def dict_to_dict(xs, cons=Dictionary):
+  y = nil
+  for k in xs:
+    y = key_ins(cons, y, k, xs[k])
+  return y
 
 # O(n)
 def list_to_list(xs):
