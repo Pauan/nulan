f5118fe32dbd7c3962962a2c1579e6ab81f9b06d
Pauan <pcxunlimited@gmail.com>
2014-01-08 22:40:56 +0800

    .

--- src/1 parse/tokenize.js
+++ src/1 parse/tokenize.js
@@ -1,180 +1,227 @@
-define(["../../lib/util/buffer", "../../lib/util/iter", "../util/data", "../util/util"], function (a, b, c, d) {
-  "use strict";
+"use strict";
 
-  var Buffer     = a.Buffer
-    , iterator   = b.iterator
-    , Number     = c.Number
-    , Symbol     = c.Symbol
-    , getSyntax  = d.getSyntax
-    , isBoxOrSym = d.isBoxOrSym
+var a = require("../../lib/util/buffer")
+  , b = require("../../lib/util/list")
+  , c = require("../../lib/util/object")
 
-  function isDelimiter(o) {
-    if (o.has()) {
-      var c = o.peek()
-      /*if (c === " " || c === "\n") {
-        return true
-      } else {*/
-      var x = getSyntax(c)
-      return x !== null && x.delimiter
-      //}
-    } else {
-      return true
-    }
-  }
-  
-  function isWhitespace(s) {
-    /*if (s === " " || s === "\n") {
-      return true
-    } else {*/
-    var x = getSyntax(s)
-    if (x !== null) {
-      return !!x.whitespace
-    } else {
-      return false
-    }
-    //}
-  }
+var buffer   = a.buffer
+  , loc      = a.loc
+  , Error    = a.Error
+  , toCons   = b.toCons
+  , cons     = b.cons
+  , car      = b.car
+  , cdr      = b.cdr
+  , nil      = b.nil
+  , isString = c.isString
+  , isNumber = c.isNumber
 
-  function numOrSym(o, info) {
-    var s = o.position()
-      , r = []
-      , x
-    while (o.has() && !isDelimiter(o, info)) {
-      r.push(o.read())
-    }
-    r = r.join("")
-    if (/^[0-9]+$/.test(r)) {
-      x = new Number(+r)
-      x.whitespace = info.whitespace
-      info.whitespace = false
-    } else {
-      x = new Symbol(r)
-      x.whitespace = info.whitespace
-      info.whitespace = isWhitespace(r)
-    }
-    x.loc = o.loc(s, o.position())
-    return x
-  }
-  
-  function one(o, info) {
-    if (!o.has()) {
-      // TODO better error message; probably use SyntaxError
-      throw new Error("unexpected end of input")
-    }
-    if (isDelimiter(o, info)) {
-      var s = o.position()
-        , y = new Symbol(o.read())
-      y.loc = o.loc(s, o.position())
-      y.whitespace = info.whitespace
-      info.whitespace = true // TODO !!x.whitespace ?
-      return y
-    } else {
-      return numOrSym(o, info)
-    }
-  }
-  
-  function findIndent(o, info) {
-    // TODO don't hardcode space and newline ?
-    while (o.has() && (o.peek() === " " || o.peek() === "\n")) {
-      o.read()
-      info.whitespace = true
-    }
-    return o.position()
-  }
 
-  // TODO hacky
-  // TODO is this correct ?
-  function popEndAt(y, endAt) {
-    var i = 0
-    while (endAt.length && i < y.length) {
-      var last = endAt[endAt.length - 1]
-      if (isBoxOrSym(y[i]) && y[i].value === last) {
-        endAt.pop()
-      }
-      ++i
-    }
-  }
-  
-  function getNext(o, info, endAt) {
-    var c = o.peek()
-      , x = getSyntax(c)
-      , y
-    // TODO: multi-character tokenize and delimiter
-    if (x !== null) {
-      if (x.tokenize != null) {
-        y = x.tokenize(o, info)
-      } else {
-        y = [one(o, info)]
-      }
-      if (x.endAt != null) {
-        endAt.push(x.endAt)
-      }
-      info.whitespace = !!x.whitespace
-    } else {
-      y = [one(o, info)]
-    }
-    popEndAt(y, endAt)
-    return y
-  }
+exports.Error = Error
 
-  // TODO multi-character tokenize and delimiter
-  function tokenize1(o) {
-    var a = []
-    
-    var endAt = []
-      , info  = { whitespace: true }
-      , start = findIndent(o, info)
-    
-    while (o.has()) {
-      var next = findIndent(o, info)
-      if (start.line === next.line   ||
-          next.column > start.column ||
-          endAt.length) {
-        a = a.concat(getNext(o, info, endAt))
-      } else {
+function Symbol(x, loc) {
+  if (loc == null) {
+    loc = null
+  }
+  this.type = "Identifier"
+  this.name = x
+  this.loc  = loc
+}
+Symbol.prototype.toString = function () {
+  return this.name
+}
+exports.Symbol = Symbol
+
+function Literal(x, loc) {
+  if (loc == null) {
+    loc = null
+  }
+  this.type  = "Literal"
+  this.value = x
+  this.loc   = loc
+}
+Literal.prototype.toString = function () {
+  if (isString(this.value)) {
+    // TODO unprintable characters too
+    return "\"" + this.value.replace(/[\\\"]/g, "\\$&") + "\""
+  } else if (isNumber(this.value)) {
+    return "" + this.value
+  } else {
+    return "" + this.value
+  }
+}
+exports.Literal = Literal
+
+
+var rules = {}
+
+function isDelimiter(s) {
+  return rules[s] != null && rules[s].delimiter
+}
+
+function tokenizeAtom(top, s, a) {
+  var r = [s.value]
+
+  var posFirst = s.loc
+    , posLast  = posFirst
+
+  if (!isDelimiter(s.value)) {
+    while (true) {
+      if (a === nil) {
         break
+      } else {
+        s = car(a)
+        if (isDelimiter(s.value)) {
+          break
+        } else {
+          r.push(s.value)
+          posLast = s.loc
+          a = cdr(a)
+        }
       }
     }
-    
+  }
+
+  r = r.join("")
+
+  if (/^[0-9]+$/.test(r)) {
+    s = new Literal(+r, loc(posFirst, posLast))
+    //x.whitespace = info.whitespace
+    //info.whitespace = false
+  } else {
+    s = new Symbol(r, loc(posFirst, posLast))
+    //x.whitespace = info.whitespace
+    //info.whitespace = isWhitespace(r)
+  }
+
+  top.push(s)
+  return a
+}
+
+
+var ends = []
+
+function endAt(s) {
+  return function (top, token, a) {
+    top.push(new Symbol(token.value, token.loc))
+    ends.push(s)
+    try {
+      while (true) {
+        if (a === nil) {
+          throw new Error(token, "missing ending " + s)
+        } else {
+          var x = car(a)
+          if (x.value === s) {
+            top.push(new Symbol(x.value, x.loc))
+            a = cdr(a)
+            break
+          } else {
+            a = tokenize1(top, a)
+          }
+        }
+      }
+    } finally {
+      ends.pop()
+    }
     return a
   }
+}
 
-  function tokenize(x, filename) {
-    var o = new Buffer(x, filename)
-
-    var oIter = {}
-    oIter[iterator] = function () {
-      return this
+function startAt(sStart) {
+  return function (top, token) {
+    if (ends.length) {
+      throw new Error(token, "expected " + ends[ends.length - 1] + " but got " + token.value)
+    } else {
+      throw new Error(token, "missing starting " + sStart)
     }
-    oIter.next = function () {
-      // TODO a little hacky
-      var x = []
-      while (x.length === 0) {
-        if (!o.has()) {
-          return { done: true }
+  }
+}
+
+function tokenizeString(s) {
+  return null
+}
+
+function tokenizeWhitespace(top, s, a) {
+  return a
+}
+
+function tokenizeComment1(token, a) {
+
+}
+
+function tokenizeComment(top, token, a) {
+  if (a === nil) {
+    return a
+  } else {
+    var x = car(a)
+    if (x.value === "|") {
+      return tokenizeComment1(token, cdr(a))
+    } else {
+      while (true) {
+        if (a === nil) {
+          break
+        } else {
+          x = car(a)
+          a = cdr(a)
+          if (x.value === "\n") {
+            break
+          }
         }
-        x = tokenize1(o)
       }
-      return { value: x }
+      return a
     }
-    return oIter
   }
-  
-  function tokenizeBrackets(o, info) {
-    var stack = []
-      , r     = []
-    while (o.has()) {
-      // TODO is this correct ?
-      r = r.concat(getNext(o, info, stack))
-      if (stack.length === 0) {
-        break
-      }
-    }
-    return r
-  }
+}
 
-  return {
-    tokenize: tokenize,
-    tokenizeBrackets: tokenizeBrackets,
+function tokenize1(top, a) {
+  if (a === nil) {
+    return a
+  } else {
+    var s = car(a)
+      , c = s.value
+
+    if (rules[c] != null && rules[c].tokenize != null) {
+      a = rules[c].tokenize(top, s, cdr(a))
+
+    } else {
+      a = tokenizeAtom(top, s, cdr(a))
+    }
+
+    return a
   }
-})
+}
+
+function tokenize(x, filename) {
+  var a = toCons(buffer(x, filename))
+  var r = []
+  while (a !== nil) {
+    a = tokenize1(r, a)
+  }
+  return r
+}
+exports.tokenize = tokenize
+
+
+function delimiter(s, f) {
+  rules[s] = { delimiter: true, tokenize: f }
+}
+
+// except (?)
+// _
+// ->
+// =
+delimiter(" ",  tokenizeWhitespace)
+delimiter("\n", tokenizeWhitespace)
+delimiter("#",  tokenizeComment)
+delimiter("\"", tokenizeString("\""))
+
+delimiter(":")
+delimiter(".")
+delimiter("`")
+delimiter(",")
+delimiter("@")
+delimiter("(")
+delimiter(")")
+delimiter("[")
+delimiter("]")
+delimiter("{")
+delimiter("}")
