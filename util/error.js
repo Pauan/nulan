bd3ec36094c53ac165d12d2699f1a810d23fe719
Pauan <pcxunlimited@gmail.com>
2016-01-09 02:00:38 -1000

    More reorganization of utils

--- util/error.js
+++ util/error.js
@@ -1,3 +1,43 @@
+import { eol } from "./node";
+import { repeat } from "./string";
+
+
+const format_line = (x) => {
+  if (x.start.column === x.end.column) {
+    return repeat(" ", x.start.column) + "^";
+
+  } else if (x.start.column < x.end.column) {
+    return repeat(" ", x.start.column) + "^" +
+           repeat("-", x.end.column - x.start.column - 1);
+
+  } else {
+    return repeat(" ", x.end.column) +
+           repeat("-", x.start.column - x.end.column) + "^";
+  }
+};
+
+const format_message = (header, x, message) => {
+  const lines = x.lines["slice"](x.start.line, x.end.line + 1);
+
+  lines["push"](format_line(x));
+
+  return header + ": " + message +
+         "  (" + x.filename +
+         " " + (x.start.line + 1) +
+         ":" + (x.start.column + 1) +
+         ")" + eol +
+         "  " + lines["join"](eol + "  ");
+};
+
+
 export const crash = (x) => {
   throw x;
+};
+
+export const error = (x, message) => {
+  crash(new Error(format_message("Error", x, message)));
+};
+
+export const warning = (x, message) => {
+  console["warn"](format_message("Warning", x, message));
 };
