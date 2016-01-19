90dfe4eb29911096e68da2407e309d41a31c9cfb
Pauan <pcxunlimited@gmail.com>
2016-01-19 00:16:05 -1000

    Adding in another parser test

--- test/test.js
+++ test/test.js
@@ -4,11 +4,14 @@ import $task from "./ffi/task";
 //import $node from "./ffi/node";
 import $tokenize from "./parser/tokenize";
 import $parse from "./parser/parse";
+//import $compile from "./compiler/compile";
+
 
 perform_tests([
   test_group("ffi/task.js", $task),
   //test_group("ffi/node.js", test2),
 
   test_group("src/tokenize.js", $tokenize),
-  test_group("src/parse.js", $parse)
+  test_group("src/parse.js", $parse),
+  //test_group("src/compile.js", $compile)
 ]);
