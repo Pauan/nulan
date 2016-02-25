e8eb27663ce182e0b1e977e6992a5df9eb1c30d6
Pauan <pcxunlimited@gmail.com>
2016-02-24 19:47:25 -1000

    Adding in some useful Monad rewrite rules and functions

---
+++ ffi/monad.js
@@ -0,0 +1,31 @@
+import { _null } from "./types";
+
+
+// TODO is this correct ?
+export const sequential = (transform, flatten, wrap, a) => {
+  const length = a["length"];
+  const values = new Array(length);
+
+  const loop = (i) =>
+    (i < length
+      ? flatten(transform(a[i], (value) => {
+          values[i] = value;
+          return loop(i + 1);
+        }))
+      : wrap(values));
+
+  return loop(0);
+};
+
+// TODO is this correct ?
+export const sequential_null = (transform, flatten, wrap, a) => {
+  const length = a["length"];
+
+  const loop = (i) =>
+    (i < length
+      ? flatten(transform(a[i], (_) =>
+          loop(i + 1)))
+      : wrap(_null));
+
+  return loop(0);
+};
